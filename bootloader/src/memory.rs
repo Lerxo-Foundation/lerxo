91b0cd94934ac795c3eac7ddbc451d6b8a7e4de6
max <103925282+vanmassey@users.noreply.github.com>
2026-08-28 18:50:35 +0100

    Add usable UEFI memory regions

--- bootloader/src/memory.rs
+++ bootloader/src/memory.rs
@@ -1,5 +1,5 @@
 use uefi::boot;
-use uefi::mem::memory_map::{MemoryMapOwned, MemoryType};
+use uefi::mem::memory_map::{MemoryMap, MemoryMapOwned, MemoryType};
 use uefi::Status;
 
 pub fn get_memory_map() -> Result<MemoryMapOwned, Status> {
@@ -7,11 +7,17 @@ pub fn get_memory_map() -> Result<MemoryMapOwned, Status> {
         .map_err(|error| error.status())
 }
 
-pub fn is_usable(memory_type: MemoryType) -> bool {
-    matches!(
-        memory_type,
-        MemoryType::CONVENTIONAL
-            | MemoryType::BOOT_SERVICES_CODE
-            | MemoryType::BOOT_SERVICES_DATA
-    )
+pub fn usable_regions(
+    memory_map: &MemoryMapOwned,
+) -> impl Iterator<Item = (u64, u64)> + '_ {
+    memory_map
+        .entries()
+        .filter(|entry| entry.ty == MemoryType::CONVENTIONAL)
+        .map(|entry| {
+            let start = entry.phys_start;
+            let size = entry.page_count * 4096;
+            let end = start + size;
+
+            (start, end)
+        })
 }
