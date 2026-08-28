4c866ef9db551753b6626d927bbab99356517dca
max <103925282+PolarExpo@users.noreply.github.com>
2026-08-28 18:10:25 +0100

    Add Lerxo kernel entry point

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,7 +3,16 @@
 
 use core::panic::PanicInfo;
 
+/// Kernel panic handler.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// Lerxo kernel entry point.
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    lerxo_kernel::arch::x86_64::initialise();
+
     loop {}
 }
