11afc439c4bcaddfbb48cd0ccf93ec1c415ed06b
max <103925282+PolarExpo@users.noreply.github.com>
2026-08-28 18:27:45 +0100

    Add UEFI bootloader initialisation

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -4,6 +4,13 @@
 use uefi::prelude::*;
 
 #[entry]
-fn main(_image_handle: Handle, _system_table: SystemTable<Boot>) -> Status {
+fn main(
+    _image_handle: Handle,
+    mut system_table: SystemTable<Boot>,
+) -> Status {
+    uefi::helpers::init(&mut system_table).unwrap();
+
+    log::info!("Welcome to the Lerxo Bootloader!");
+
     loop {}
 }
